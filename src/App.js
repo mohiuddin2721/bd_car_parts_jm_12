beb2ecd4c90c129e449ab347cf7fe2c526f73edb
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-22 23:44:46 +0600

    daisyUi installed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>we are coming...............</h1>
+      <h1 className='text-primary'>we are coming...............</h1>
+      <button class="btn btn-primary">Button</button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,20 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#fdb819",
+          secondary: "#ff9923",
+          accent: "#212121",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
 }
