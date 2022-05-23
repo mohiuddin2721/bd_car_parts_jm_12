3e5799c1f9d7106e91fad390ed8edf5c26e4972a
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-23 22:09:12 +0600

    banner2 completed

---
+++ src/components/Home/Banner2.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import bgCar1 from '../../images/bgCar1.webp';
+// import parts1 from '../../images/parts1.webp';
+import parts9 from '../../images/parts9.webp';
+import parts6t from '../../images/parts6t.webp';
+
+const Banner2 = () => {
+    return (
+        <div style={{ backgroundImage: `url(${bgCar1})` }} className='min-h-max bg-auto'>
+            <h2 className='text-secondary text-4xl my-4 font-bold ml-8'>BEST SELL</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
+                <div>
+                    <div>
+                        <img src={parts9} className='w-48 mx-auto' alt="" />
+                    </div>
+                    <div>
+                        <p className='text-white my-2 ml-8 text-xl font-bold'>Gaido Super Alternator</p>
+                        <p className='text-white ml-8'>Aluminum extrusion in Italy reaches very high standards, so much so that it is appreciated, and therefore in demand, from all over the world</p>
+                        <p className='text-white ml-8'>Price: $550</p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src={parts6t} className='w-48 mx-auto' alt="" />
+                    </div>
+                    <div>
+                        <p className='text-white my-2 ml-8 text-xl font-bold'>CAR HEADLIGHTS</p>
+                        <p className='text-white ml-8'>Automotive lighting industry. LED reflectors. Electronics manufacturers. Household products. RayVac specializes in applying a simulated chrome finish to non-porous base materials</p>
+                        <p className='text-white ml-8'>Price: $570</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner2;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner1 from './Banner1';
+import Banner2 from './Banner2';
 import BsSummary from './BsSummary';
 import CarParts1 from './CarParts1';
 import Parts from './Parts';
@@ -11,6 +12,7 @@ const Home = () => {
             <CarParts1></CarParts1>
             <Parts></Parts>
             <BsSummary></BsSummary>
+            <Banner2></Banner2>
         </div>
     );
 };
