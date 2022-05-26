cb6a8ab376f89d07ef9e8a36154b273530d7d47e
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-26 21:11:06 +0600

    react query setUp on index.js

--- src/components/Dashboard/MakeAdmin.js
+++ src/components/Dashboard/MakeAdmin.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 
 const MakeAdmin = () => {
+    const {data: users, isLoading, reFetch} = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()));
+
+    if(isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h1>Make admin...........</h1>
+            <h1>Make admin...........{users.length}</h1>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+
   </React.StrictMode>
 );
 
