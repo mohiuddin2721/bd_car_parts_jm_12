0400313b6a5c0dcc39b27c69db48c1dce48f9e8c
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-27 02:41:00 +0600

    makeAdmin working

--- src/components/Dashboard/MakeAdmin.js
+++ src/components/Dashboard/MakeAdmin.js
@@ -1,35 +1,48 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import MakeAdminRow from './MakeAdminRow';
 
 const MakeAdmin = () => {
-    const { data: users, isLoading, reFetch } = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()));
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
 
     if (isLoading) {
         return <Loading></Loading>
     }
     return (
         <div>
-            <h1>All Users:-{users.length}</h1>
-            <div class="overflow-x-auto">
-                <table class="table w-full">
+            <h1>All Users:-{users?.length}</h1>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
                     <thead>
                         <tr>
                             <th></th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Users</th>
+                            <th>Admin</th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
-                        
+                        {
+                            users?.map((user, index) => <MakeAdminRow
+                                key={user._id}
+                                user={user}
+                                index={index}
+                                refetch={refetch}
+                            ></MakeAdminRow>)
+                        }
                     </tbody>
                     <tfoot>
                         <tr>
                             <th></th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Users</th>
+                            <th>Admin</th>
+                            <th>Delete</th>
                             <th></th>
                         </tr>
                     </tfoot>
--- src/components/Dashboard/MakeAdminRow.js
+++ src/components/Dashboard/MakeAdminRow.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { RiAdminLine } from "react-icons/ri";
+import { AiFillDelete } from "react-icons/ai";
+import { toast } from 'react-toastify';
 
-const MakeAdminRow = () => {
+
+const MakeAdminRow = ({ user, index, refetch }) => {
+    const { email, role } = user;
+
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if(res.status === 403) {
+                    toast.error('Failed to an admin, You are not admin')
+                }
+                return res.json()})
+            .then(data => {
+                // console.log(data);
+                if (data.modifiedCount > 0) {
+                    refetch();
+                    toast.success('Successfully made an admin');
+                }
+            })
+    }
     return (
-        <tr>
-            <th>1</th>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
+        <tr className="hover">
+            <th>{index + 1}</th>
+            <td>{email}</td>
+            <td>{role !== 'admin' && <button onClick={makeAdmin} className="btn btn-xs btn-secondary">Make Admin  <RiAdminLine className='text-white text-sm' /></button>}</td>
+            <td><button className="btn btn-xs btn-accent"><AiFillDelete className='text-red-500 text-sm' /></button></td>
         </tr>
     );
 };
