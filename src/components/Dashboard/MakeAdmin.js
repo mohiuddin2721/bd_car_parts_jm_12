ddd989eb923839460f9ccd9cbb2ac13788b1f91b
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-27 00:40:37 +0600

    make admin row add

--- src/components/Dashboard/MakeAdmin.js
+++ src/components/Dashboard/MakeAdmin.js
@@ -3,14 +3,38 @@ import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
 
 const MakeAdmin = () => {
-    const {data: users, isLoading, reFetch} = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()));
+    const { data: users, isLoading, reFetch } = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()));
 
-    if(isLoading) {
+    if (isLoading) {
         return <Loading></Loading>
     }
     return (
         <div>
-            <h1>Make admin...........{users.length}</h1>
+            <h1>All Users:-{users.length}</h1>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
         </div>
     );
 };
---
+++ src/components/Dashboard/MakeAdminRow.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MakeAdminRow = () => {
+    return (
+        <tr>
+            <th>1</th>
+            <td>Cy Ganderton</td>
+            <td>Quality Control Specialist</td>
+            <td>Blue</td>
+        </tr>
+    );
+};
+
+export default MakeAdminRow;
