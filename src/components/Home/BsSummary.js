d54980ea9904b4c4529337cf69ab4cec345ea682
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-08-08 23:30:12 +0600

    business summary changed

--- src/components/Home/BsSummary.js
+++ src/components/Home/BsSummary.js
@@ -1,36 +1,121 @@
 import React from 'react';
-import { FaSortAmountUp, FaUsers, FaTools } from "react-icons/fa";
-import { FcLike } from "react-icons/fc";
+import VisibilitySensor from "react-visibility-sensor";
+import CountUp from "react-countup";
+import { FaUsers } from "react-icons/fa";
+import { BsFlagFill } from "react-icons/bs";
+import { GiDeliveryDrone } from "react-icons/gi";
+import { AiFillLike } from "react-icons/ai";
 
 const BsSummary = () => {
+
     return (
-        <div className='my-16'>
-            <h1 className='text-primary text-center text-2xl font-bold'>MILLIONS BUSINESS TRUST US</h1>
-            <p className='text-center'>TRY TO UNDERSTAND USERS EXPECTATION</p>
-            {/* <div className="divider w-48 items-center mx-auto mt-0">  </div> */}
-            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-2 pt-4'>
+        <section>
+            <div className="py-12 px-5">
+                <h1 className='section-title text-2xl md:text-4xl font-bold'>We Delivered car parts All Over The World</h1>
                 <div>
-                    <FaUsers className='text-8xl mx-auto' />
-                    <p className='text-center font-bold text-2xl text-secondary'>109</p>
-                    <p className='text-center text-xl'><small>Countries</small></p>
-                </div>
-                <div>
-                    <FaSortAmountUp className='text-8xl mx-auto' />
-                    <p className='text-center font-bold text-2xl text-secondary'>150M</p>
-                    <p className='text-center text-xl'><small>Annual Revenue</small></p>
-                </div>
-                <div>
-                    <FaTools className='text-8xl mx-auto' />
-                    <p className='text-center font-bold text-2xl text-secondary'>60+</p>
-                    <p className='text-center text-xl'><small>Tools</small></p>
-                </div>
-                <div>
-                    <FcLike className='text-8xl mx-auto' />
-                    <p className='text-center font-bold text-2xl text-secondary'>63k</p>
-                    <p className='text-center text-xl'><small>Reviews</small></p>
+                    <div className="summery-inside-banner">
+                        <div className="grid grid-cols-1 py-10 gap-5 md:grid-cols-2 lg:grid-cols-4">
+                            <div className="lg:mx-10 rounded-2xl p-4 bg-white text-gray-500 hover:text-primary flex flex-col justify-center">
+                                <div className="text-6xl py-5 mb-2 px-[1.45rem] cursor-text mx-auto transition-all duration-300">
+                                    
+                                <BsFlagFill className='text-8xl mx-auto' />
+                                </div>
+                                <div className="text-center">
+                                    <CountUp start={0} end={49} duration={2}>
+                                        {({ countUpRef, start }) => (
+                                            <VisibilitySensor onChange={start} delayedCall>
+                                                <span
+                                                    className="text-4xl my-2 text-primary font-semibold"
+                                                    ref={countUpRef}
+                                                />
+                                            </VisibilitySensor>
+                                        )}
+                                    </CountUp>
+                                    <span className="text-4xl my-2 text-primary font-semibold">
+                                        +
+                                    </span>
+                                    <p className="text-2xl font-semibold my-2 uppercase font-mono">
+                                        TOTAL AWARDS
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="lg:mx-10 rounded-2xl p-4 bg-white text-gray-500 hover:text-primary  flex flex-col justify-center">
+                                <div className="text-6xl py-5 mb-2 px-[1.45rem] cursor-text mx-auto transition-all duration-300">
+                                    
+                                    <GiDeliveryDrone className='text-8xl mx-auto' />
+                                </div>
+                                <div className="text-center">
+                                    <CountUp start={0} end={500} duration={2}>
+                                        {({ countUpRef, start }) => (
+                                            <VisibilitySensor onChange={start} delayedCall>
+                                                <span
+                                                    className="text-4xl my-2 text-primary font-semibold"
+                                                    ref={countUpRef}
+                                                />
+                                            </VisibilitySensor>
+                                        )}
+                                    </CountUp>
+                                    <span className="text-4xl my-2 text-primary font-semibold">
+                                        K+
+                                    </span>
+                                    <p className="text-2xl font-semibold my-2 uppercase font-mono">
+                                        TOTAL deliveries
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="lg:mx-10 rounded-2xl p-4 bg-white text-gray-500 hover:text-primary  flex flex-col justify-center">
+                                <div className="text-6xl py-5 mb-2 px-[1.45rem] cursor-text mx-auto transition-all duration-300">
+                                    
+                                    <FaUsers className='text-8xl mx-auto' />
+                                </div>
+                                <div className="text-center">
+                                    <CountUp start={0} end={70} duration={2}>
+                                        {({ countUpRef, start }) => (
+                                            <VisibilitySensor onChange={start} delayedCall>
+                                                <span
+                                                    className="text-4xl my-2 text-primary font-semibold"
+                                                    ref={countUpRef}
+                                                />
+                                            </VisibilitySensor>
+                                        )}
+                                    </CountUp>
+                                    <span className="text-4xl my-2 text-primary font-semibold">
+                                        K+
+                                    </span>
+                                    <p className="text-2xl font-semibold my-2 uppercase font-mono">
+                                        Happy clients
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="lg:mx-10 rounded-2xl p-4 bg-white text-gray-500 hover:text-primary   flex flex-col justify-center">
+                                <div className="text-6xl py-5 mb-2 px-[1.45rem] cursor-text mx-auto transition-all duration-300">
+                                    
+                                    <AiFillLike className='text-8xl mx-auto' />
+                                </div>
+                                <div className="text-center">
+                                    <CountUp start={0} end={10} duration={2}>
+                                        {({ countUpRef, start }) => (
+                                            <VisibilitySensor onChange={start} delayedCall>
+                                                <span
+                                                    className="text-4xl my-2 text-primary font-semibold"
+                                                    ref={countUpRef}
+                                                />
+                                            </VisibilitySensor>
+                                        )}
+                                    </CountUp>
+                                    <span className="text-4xl my-2 text-primary font-semibold">
+                                        K+
+                                    </span>
+                                    <p className="text-2xl font-semibold my-2 uppercase font-mono">
+                                        positive Feedbacks
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
