e1bbecdcdbc17ff5d151e46dfce5046124288ec3
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-08-16 02:53:54 +0600

    icon add in dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { FaRunning } from 'react-icons/fa';
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import auth from '../../firebase.init';
 import useAdmin from '../../hooks/useAdmin';
@@ -19,13 +20,67 @@ const Dashboard = () => {
                 <label htmlFor="sidebar-dashboard" className="drawer-overlay"></label>
                 <ul className="menu p-4 overflow-y-auto sm:w-52 w-1/2 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
-                    <li><Link to='/dashboard' className='font-bold'>My Orders</Link></li>
-                    {!admin && <li><Link to='/dashboard/addReview' className={`font-bold ${pathname.includes('/dashboard/addReview') && 'bg-[#fdb91c]'}`}>Add A Review</Link></li>}
-                    <li><Link to='/dashboard/myProfile' className={`font-bold ${pathname.includes('/dashboard/myProfile') && 'bg-[#fdb91c]'}`}>My Profile</Link></li>
-                    {admin && <li><Link to='/dashboard/manageOrders' className={`font-bold ${pathname.includes('/dashboard/manageOrders') && 'bg-[#fdb91c]'}`}>Manage orders</Link></li>}
-                    {admin && <li><Link to='/dashboard/addProduct' className={`font-bold ${pathname.includes('/dashboard/addProduct') && 'bg-[#fdb91c]'}`}>Add Product</Link></li>}
-                    {admin && <li><Link to='/dashboard/makeAdmin' className={`font-bold ${pathname.includes('/dashboard/makeAdmin') && 'bg-[#fdb91c]'}`}>Make Admin</Link></li>}
-                    {admin && <li><Link to='/dashboard/manageItems' className={`font-bold ${pathname.includes('/dashboard/manageItems') && 'bg-[#fdb91c]'}`}>Manage Items</Link></li>}
+                    <li>
+                        <Link
+                            to='/dashboard'
+                            className='font-bold'
+                        >
+                            My Orders<span className={`font-bold text-white ${pathname.includes('/dashboard/myProfile') && 'text-[#fff] block text-xl'}`}><FaRunning></FaRunning></span>
+                        </Link>
+                    </li>
+                    {
+                        !admin && <li>
+                            <Link to='/dashboard/addReview'
+                                className={`font-bold ${pathname.includes('/dashboard/addReview') && 'bg-[#fdb91c]'}`}
+                            >
+                                Add A Review<span className={`font-bold text-white ${pathname.includes('/dashboard/myProfile') && 'text-[#fff] block text-xl'}`}><FaRunning></FaRunning></span>
+                            </Link>
+                        </li>
+                    }
+                    <li>
+                        <Link
+                            to='/dashboard/myProfile'
+                            className={`font-bold ${pathname.includes('/dashboard/myProfile') && 'bg-[#fdb91c]'}`}
+                        >
+                            My Profile<span className={`font-bold text-white ${pathname.includes('/dashboard/myProfile') && 'text-[#fff] block text-xl'}`}><FaRunning></FaRunning></span>
+                        </Link>
+                    </li>
+                    {
+                        admin && <li>
+                            <Link to='/dashboard/manageOrders'
+                                className={`font-bold ${pathname.includes('/dashboard/manageOrders') && 'bg-[#fdb91c]'}`}
+                            >
+                                Manage orders<span className={`font-bold text-white ${pathname.includes('/dashboard/myProfile') && 'text-[#fff] block text-xl'}`}><FaRunning></FaRunning></span>
+                            </Link>
+                        </li>
+                    }
+                    {
+                        admin && <li>
+                            <Link to='/dashboard/addProduct'
+                                className={`font-bold ${pathname.includes('/dashboard/addProduct') && 'bg-[#fdb91c]'}`}
+                            >
+                                Add Product<span className={`font-bold text-white ${pathname.includes('/dashboard/myProfile') && 'text-[#fff] block text-xl'}`}><FaRunning></FaRunning></span>
+                            </Link>
+                        </li>
+                    }
+                    {
+                        admin && <li>
+                            <Link to='/dashboard/makeAdmin'
+                                className={`font-bold ${pathname.includes('/dashboard/makeAdmin') && 'bg-[#fdb91c]'}`}
+                            >
+                                Make Admin<span className={`font-bold text-white ${pathname.includes('/dashboard/myProfile') && 'text-[#fff] block text-xl'}`}><FaRunning></FaRunning></span>
+                            </Link>
+                        </li>
+                    }
+                    {
+                        admin && <li>
+                            <Link to='/dashboard/manageItems'
+                                className={`font-bold ${pathname.includes('/dashboard/manageItems') && 'bg-[#fdb91c]'}`}
+                            >
+                                Manage Items<span className={`font-bold text-white ${pathname.includes('/dashboard/myProfile') && 'text-[#fff] block text-xl'}`}><FaRunning></FaRunning></span>
+                            </Link>
+                        </li>
+                    }
                 </ul>
             </div>
         </div>
